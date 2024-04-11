f8e5abfa71bbd77a2c59f838c3ddf24956ebcb48
atomu-okada-gyawali <atomgyawali2@gmail.com>
2024-04-11 10:33:19 +0545

    week1 qn 3

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -17,6 +17,6 @@ public class Hello{
         String. Don't forget: use \"       
         instead of " !      
         '' is not the same as "  */
-        System.out.print("A \"quoted\" String is\n'much' better if you learn the rules of \"escape sequences.\"\nAlso,\"\" represents an empty String.Don't forget: use \\\" instead of \" !\n\" is not the same as \"");
+        System.out.print("A \"quoted\" String is\n'much' better if you learn\nthe rules of \"escape sequences.\"\nAlso,\"\" represents an empty\nString.Don't forget: use \\\"\ninstead of \" !\n\" is not the same as \"");
     }   
     }
