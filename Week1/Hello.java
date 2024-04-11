8c744734a3ea285f191d1cf91f6fa85dfd24371d
atomu-okada-gyawali <atomgyawali2@gmail.com>
2024-04-11 10:19:52 +0545

    qn2

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -2,5 +2,11 @@ public class Hello{
     public static void main(String[] args) {
         // 1. WAP to display Hello World.Save the program as Hello.java
         System.out.println("Hello World!");
+        /* 
+         2. WAP to display 
+            This is my first program.
+            I am on module ST4003CEM
+        */
+        System.out.print("This is my first program.\nI am on module ST4003CEM");
     }
 }
