07ea51f297b3b5a84b471c4e423870bb51f1c855
atomu-okada-gyawali <atomgyawali2@gmail.com>
2024-04-11 10:11:18 +0545

    Week 1 started

---
+++ Week1/Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+    public static void main(String[] args) {
+        // 1. WAP to display Hello World.Save the program as Hello.java
+        System.out.println("Hello World!");
+    }
+}
