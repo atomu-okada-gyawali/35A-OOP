971f7a05b504bc11881946c8d6f489fe4fe336b5
atomu-okada-gyawali <atomgyawali2@gmail.com>
2024-04-11 10:42:57 +0545

    qn 4 and 5

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -17,6 +17,40 @@ public class Hello{
         String. Don't forget: use \"       
         instead of " !      
         '' is not the same as "  */
-        System.out.print("A \"quoted\" String is\n'much' better if you learn\nthe rules of \"escape sequences.\"\nAlso,\"\" represents an empty\nString.Don't forget: use \\\"\ninstead of \" !\n\" is not the same as \"");
+        System.out.print("A \"quoted\" String is\n'much' better if you learn\nthe rules of \"escape sequences.\"\nAlso,\"\" represents an empty\nString.Don't forget: use \\\"\ninstead of \" !\n\" is not the same as \"\n");
+
+        /*4. WAP that prints the following pattern:
+
+        *
+
+        **
+
+        ***
+
+        *****/
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print('*');
+              }
+            System.out.print('\n');
+          }
+        /*5. WAP prints the following pattern:
+
+        *********
+
+        *********
+
+        *********
+
+        *********
+
+        */
+        System.out.println("");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= 5; j++) {
+                System.out.print('*');
+              }
+            System.out.print('\n');
+          }
     }   
     }
