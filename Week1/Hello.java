9756fb7ad1a12784ec781d1b1d08daafd2ed0d3d
atomu-okada-gyawali <atomgyawali2@gmail.com>
2024-04-11 10:31:02 +0545

    Week1 tasks3

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -7,6 +7,16 @@ public class Hello{
             This is my first program.
             I am on module ST4003CEM
         */
-        System.out.print("This is my first program.\nI am on module ST4003CEM");
+        System.out.print("This is my first program.\nI am on module ST4003CEM\n");
+        /*
+        3.Write a program that prints the following output:
+        A "quoted" String is
+        'much' better if you learn     
+        the rules of "escape sequences."       
+        Also, "" represents an empty       
+        String. Don't forget: use \"       
+        instead of " !      
+        '' is not the same as "  */
+        System.out.print("A \"quoted\" String is\n'much' better if you learn the rules of \"escape sequences.\"\nAlso,\"\" represents an empty String.Don't forget: use \\\" instead of \" !\n\" is not the same as \"");
+    }   
     }
-}
