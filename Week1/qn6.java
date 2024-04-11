42b6a11e83e987cbbcb16159a3641b49fe36ea16
atomu-okada-gyawali <atomgyawali2@gmail.com>
2024-04-11 10:56:35 +0545

    qn6

---
+++ Week1/qn6.java
@@ -0,0 +1,12 @@
+public class qn6 {
+    public static void main(String[] args) {
+        /*A well-formed Java program has a main method with { and } braces.
+        A System.out.println statement has ( and ) and usually a String that starts and ends with a " character.
+        (But we type \" instead!)
+
+        What is the difference between
+        a ' and a "?  Or between a " and a \"?
+        One is what we see when we're typing our program. The other is what appears on the "console." */
+        System.out.print("A well-formed Java program has a main method with { and } braces.\n A System.out.println statement has ( and ) and usually a String that starts has ( and) and usually a String that starts and ends with a \" character.\n(But we type\\\" instead!\n\nWhat is the difference between\n a ; and a\"? Or between a \" and a \\\"?\nOne is what we see when we're typing our program. The other is what appeears when we're typing is what appears on the \"console.\"");
+    }
+}
