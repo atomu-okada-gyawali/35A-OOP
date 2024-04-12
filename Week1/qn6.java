e788c9e0197baa577ef8390f329855f397056061
atomu-okada-gyawali <atomgyawali2@gmail.com>
2024-04-12 11:55:47 +0545

    qn6

--- Week1/qn6.java
+++ Week1/qn6.java
@@ -7,6 +7,6 @@ public class qn6 {
         What is the difference between
         a ' and a "?  Or between a " and a \"?
         One is what we see when we're typing our program. The other is what appears on the "console." */
-        System.out.print("A well-formed Java program has a main method with { and } braces.\n A System.out.println statement has ( and ) and usually a String that starts has ( and) and usually a String that starts and ends with a \" character.\n(But we type\\\" instead!\n\nWhat is the difference between\n a ; and a\"? Or between a \" and a \\\"?\nOne is what we see when we're typing our program. The other is what appeears when we're typing is what appears on the \"console.\"");
+        System.out.print("A well-formed Java program has a main method with { and } braces.\n A System.out.println statement has ( and ) and usually a String that starts has ( and) and usually a String that starts and ends with a \" character.\n(But we type\\\" instead!\n\nWhat is the difference between\na ; and a\"? Or between a \" and a \\\"?\nOne is what we see when we're typing our program. The other is what appeears when we're typing is what appears on the \"console.\"");
     }
 }
